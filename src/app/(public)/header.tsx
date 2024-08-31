151ca9a60668f8d0265445ab3c046069e0f8012d
svaponi <10941963+svaponi@users.noreply.github.com>
2024-08-31 16:06:09 +0200

    task: fix header

--- src/app/(public)/header.tsx
+++ src/app/(public)/header.tsx
@@ -15,29 +15,38 @@ export default async function Header() {
   const comp = user ? <UserMenu user={user} /> : <LoginButton />;
   return (
     <div className="border-b">
-      <div className="container mx-auto flex max-w-7xl items-center justify-end p-4 md:justify-between md:px-6">
-        <nav className="hidden items-center space-x-4 text-sm md:flex">
-          <Link className="flex items-center gap-2 font-semibold" href="/">
-            <SawbladeIcon className="h-6 w-6" />
-            <span className="">Sawblade</span>
-          </Link>
-          {links.map((link) => (
-            <Link className="" href={link.href} key={link.title}>
-              {link.title}
+      <div className="container mx-auto max-w-7xl">
+        <div className="hidden justify-between p-4 px-6 md:flex">
+          <nav className="flex items-center space-x-4 text-sm">
+            <Link className="flex items-center gap-2 font-semibold" href="/">
+              <SawbladeIcon className="h-6 w-6" />
+              <span className="">Sawblade</span>
             </Link>
-          ))}
-        </nav>
-        <div className="hidden items-center space-x-4 md:flex">
-          <ThemeToggle />
-          {comp}
+            {links.map((link) => (
+              <Link className="" href={link.href} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+          <div className="flex items-center space-x-4">
+            <ThemeToggle />
+            {comp}
+          </div>
         </div>
-        <div className="flex items-center space-x-4 md:hidden">
-          <ThemeToggle />
-          {comp}
-          <button className="inline-flex rounded-md md:hidden" type="button">
+        <div className="flex items-center justify-between p-4 md:hidden">
+          {/* TODO add drawer menu */}
+          <Link
+            href="/"
+            className="inline-flex rounded-md md:hidden"
+            type="button"
+          >
             <MenuIcon className="h-6 w-6" />
             <span className="sr-only">Toggle Menu</span>
-          </button>
+          </Link>
+          <div className="flex items-center gap-4">
+            <ThemeToggle />
+            {comp}
+          </div>
         </div>
       </div>
     </div>
